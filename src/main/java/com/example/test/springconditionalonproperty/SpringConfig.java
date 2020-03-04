da81e98ad83ce5bb5527a6c3bbc8d81b5d0209ee
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2020-03-04 10:22:18 +0800

    Added new scenarios for dynamic values

--- src/main/java/com/example/test/springconditionalonproperty/SpringConfig.java
+++ src/main/java/com/example/test/springconditionalonproperty/SpringConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class SpringConfig {
 
     // NOTE: "If you do not configure the property in properties file and havingValue = "true" with matchIfMissing = true does not make sense."
-    // Means if you're using havingValue = "true", that means your MUST need to have a property value in *.properties file.
+    // Means if you're using havingValue = "true", that means your MUST need to have a property value in *.properties file with the correct property name with value "true".
     // BUT, you have added matchIfMissing = true. So no matter your *.properties file has the value or not, the FIRST and ONLY thing that Spring Boot cares is havingValue.
     // That's the reason why havingValue = "true" with matchIfMissing = true does not make sense
 
@@ -36,11 +36,29 @@ public class SpringConfig {
     // If havingValue = true, but module.enabled=false --> Not loaded, found different value in property
 
     // Scenario 4:
-    @ConditionalOnProperty(prefix = "module", name = "enabled", havingValue = "true", matchIfMissing = true)
+//    @ConditionalOnProperty(prefix = "module", name = "enabled", havingValue = "true", matchIfMissing = true)
     // Assume matchIfMissing = true,
     // If havingValue = true, but there's no module.enabled property --> Loaded successfully
     // If havingValue = true, but module.enabled=true --> Loaded successfully
     // If havingValue = true, but module.enabled=false --> Not loaded, found different value in property
+
+    // Dynamic values
+    // Scenario 5:
+//    @ConditionalOnProperty(prefix = "module", name = {"enabled", "dynamic"})
+    // If module.enabled=true but no module.dynamic --> Not loaded, could not find property module.dynamic
+    // If module.enabled=true but module.dynamic=true --> Loaded successfully
+    // If module.enabled=true but module.dynamic=false --> Not loaded, found different value in property module.dynamic
+    // If module.enabled=false but module.dynamic=true --> Not loaded, found different value in property module.enabled
+    // If module.enabled=false but module.dynamic=false --> Not loaded, found different value in property module.enabled & module.dynamic
+
+    // Scenario 6:
+//    @ConditionalOnProperty(prefix = "module", name = {"enabled", "dynamic"}, havingValue = "true")
+    // Assume havingValue = "true"
+    // If module.enabled=true but module.dynamic=false --> Not loaded, found different value in property module.dynamic
+    // If module.enabled=false but module.dynamic=true --> Not loaded, found different value in property module.enabled
+    // If module.enabled=false but module.dynamic=false --> Not loaded, found different value in property module.enabled & module.dynamic
+
+    @ConditionalOnProperty(prefix = "module", name = {"enabled", "dynamic"}, matchIfMissing = true)
 
     public SpringService springService() {
         return new SpringService();
